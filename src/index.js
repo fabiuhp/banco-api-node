ac2f97be6164ae21c996a21abbd785814d9472e9
Fábio Pereira <fabiuhp@msn.com>
2021-12-10 23:32:21 -0300

    delete

--- src/index.js
+++ src/index.js
@@ -110,4 +110,28 @@ app.get("/statement/date", verifyIfExistAccountCPF, (req, res) => {
     return res.json(statement);
 });
 
+app.put("/account", verifyIfExistAccountCPF, (req, res) => {
+    const { name } = req.body;
+    const { customer } = req;
+
+    customer.name = name;
+
+    return res.status(200).send();
+});
+
+app.get("/account", verifyIfExistAccountCPF, (req, res) => {
+    const { customer } = req;
+
+    return res.json(customer);
+});
+
+app.delete("/account", verifyIfExistAccountCPF, (req, res) => {
+    const { customer } = req;
+
+    // splice
+    customers.splice(customer, 1);
+
+    return res.status(200).json(customers);
+});
+
 app.listen(3333);
